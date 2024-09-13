12bd33356e08c5953120a1e715068587f4c10446
ricocanuto <ricardomoraescanuto@gmail.com>
2024-09-13 18:36:45 -0300

    ID-update

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,8 +1,15 @@
 import { Sequelize, Model } from 'sequelize';
+import { v4 as uuidv4 } from 'uuid';
 
 class User extends Model {
     static init(sequelize) {
         super.init({
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: uuidv4, // Gerar automaticamente um UUID
+                allowNull: false,
+                primaryKey: true,
+            },
             name: Sequelize.STRING,
             email: Sequelize.STRING,
             password_hash: Sequelize.STRING,
