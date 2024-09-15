3ba6a405e50916f0541372c2fd161cfbdbe815c5
ricocanuto <ricardomoraescanuto@gmail.com>
2024-09-15 17:39:40 -0300

    email-and-password

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,28 +4,53 @@
 //  * show => Listar apenas um
 //  * update => Atualizar
 //  * delete => Deletar
- 
+
 
 import { v4 } from 'uuid';
+import * as Yup from 'yup';
 
 import User from '..//models/User';
 
 class UserController {
-    async store(request, response) {
-        const {name, email, password_hash, admin } = request.body;
+  async store(request, response) {
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password_hash: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
+    });
 
-        const user = await User.create({
-            name,
-            email,
-            password_hash,
-            admin,
-      });
-      
-      return response.status(201).json({
-        id: user.id,
-        name,
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { name, email, password_hash, admin } = request.body;
+
+    const userExists = await User.findOne({
+      where: {
         email,
-        admin,
+      },
+    });
+
+    if(userExists){
+      return response.status(400).json({ error: 'User already exists' });
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    });
+
+    return response.status(201).json({
+      id: user.id,
+      name,
+      email,
+      admin,
     });
   }
 }
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,24 +1,28 @@
-import { Sequelize, Model } from 'sequelize';
-import { v4 as uuidv4 } from 'uuid';
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcrypt';
 
 class User extends Model {
     static init(sequelize) {
-        super.init({
-            id: {
-                type: Sequelize.UUID,
-                defaultValue: uuidv4, // Gerar automaticamente um UUID
-                allowNull: false,
-                primaryKey: true,
+        super.init(
+            {
+                name: Sequelize.STRING,
+                email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
+                password_hash: Sequelize.STRING,
+                admin: Sequelize.BOOLEAN,
             },
-            name: Sequelize.STRING,
-            email: Sequelize.STRING,
-            password_hash: Sequelize.STRING,
-            admin: Sequelize.BOOLEAN,
-        },
-        {
-            sequelize,
-        },    
+            {
+                sequelize,
+            },
         );
+
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                user.password_hash = await bcrypt.hash(user.password, 10);
+            }
+        });
+
+        return this;
     }
 }
 
