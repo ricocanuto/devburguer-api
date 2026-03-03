cd328bb5698586d00c5b6fd63ec754a0deaeb108
ricocanuto <ricardomoraescanuto@gmail.com>
2026-03-03 18:22:08 -0300

    update-db

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
-import express from 'express';
-import routes from './routes.js';
-import { resolve } from 'node:path';
+import express from "express";
+import routes from "./routes.js";
+import { resolve } from "node:path";
 
-import './database';
+import "./database";
 
 class App {
-    constructor() {
-        this.app = express();
+  constructor() {
+    this.app = express();
 
-        this.middlewares();
-        this.routes();
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    middlewares() {
-        this.app.use(express.json());
-        this.app.use('/product-file', express.static(resolve(__dirname, '..', 'uploads')));
-    }
+  middlewares() {
+    this.app.use(express.json());
+    this.app.use("/product-file", express.static(resolve(__dirname, "..", "uploads")));
+  }
 
-    routes() {
-        this.app.use(routes);
-
-
-    }
+  routes() {
+    this.app.use(routes);
+  }
 }
 export default new App().app;
