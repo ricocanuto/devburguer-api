2272c5e749d5034fcaa747ef8eb081a9b3f38350
ricocanuto <ricardomoraescanuto@gmail.com>
2026-06-24 11:01:06 -0300

    corret-err

--- src/config/database.js
+++ src/config/database.js
@@ -2,23 +2,22 @@ import 'dotenv/config';
 
 const config = process.env.DATABASE_URL || {
   dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    dialectOptions: process.env.NODE_ENV === 'production' ? {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false, // Permite a conexão segura da Render para o Supabase
-        
-        }
-    },
+  },
 };
 
 export default config;
