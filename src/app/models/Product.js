8c6af3aacf36aa00ac47f1d36ba54cd58c763096
ricocanuto <ricardomoraescanuto@gmail.com>
2026-07-01 22:09:17 -0300

    underscored:true

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -17,6 +17,7 @@ class Product extends Model {
             {
                 sequelize,
                 tableName: 'products',
+                underscored: true,
             },
         );
     
